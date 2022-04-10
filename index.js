6444dd71e16e692c4bbf3affa760451f96835ac8
Walter Schemith <wascasla@gmail.com>
2022-04-10 20:31:44 -0300

    Add port

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const mongoString = process.env.DATABASE_URL;
 const routes = require('./routes/routes');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
 
 
 mongoose.connect(mongoString);
@@ -26,6 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/api', routes)
 
-app.listen(5000, () => {
-  console.log(`Server Started at ${5000}`)
+app.listen(port, () => {
+  console.log(`Server Started at ${port}`)
 })
